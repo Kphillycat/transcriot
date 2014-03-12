153c78d3c04d6124045dbde72d4fad160e44f789
Kate <katepdonahue@gmail.com>
2014-03-12 17:25:56 -0400

    Update accessible methods

--- app/controllers/claims_controller.rb
+++ app/controllers/claims_controller.rb
@@ -29,7 +29,8 @@ class ClaimsController < ApplicationController
   def update
     claim = Claim.find(params[:id]) 
     claim.update_attributes(params[:claim])
-    claim.update_claimant(person_params)
+    claim.update_claimant(claimant_params)
+    debugger
     claim.update_examiner(examiner_params)
     claim.update_damages(damages_params)
 
@@ -40,8 +41,12 @@ class ClaimsController < ApplicationController
     params.require(:claim).permit(:claim_date, :incident_date, :resolution_date, :claim_number, :record_number, :incident_address, :incident_address_type, :total_claimed, :total_awarded, :examiners)
   end
 
+  def claimant_params
+    params[:people][:claimant].require(:person).permit(:name, :previous_address, :current_address, :role, :gender, :race, :claim_id)
+  end
+
   def person_params
-    params.require(:person).permit(:name, :previous_address, :current_address, :role, :gender, :race, :claim_id)
+    params[:people][:affidavit].require(:person).permit(:name, :previous_address, :current_address, :role, :gender, :race, :claim_id)
   end
 
   def examiner_params
