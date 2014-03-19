78b250935895855a962376daeac7fabff24a4d62
Kphillycat <keithwilliamsjr@gmail.com>
2014-03-19 15:56:06 -0400

    Remove window load on hocr_toggle.js

--- app/assets/javascripts/hocr_toggle.js
+++ app/assets/javascripts/hocr_toggle.js
@@ -1,19 +1,19 @@
 $(document).ready( function() {
-  setTimeout(function(){
-    var $ocrx_word = $('.ocrx_word');
-  
-    $('.hocr_toggle').on("click", function() {
-      if($ocrx_word.hasClass('transparent')) {
-        $ocrx_word.removeClass('transparent');
-      }
-      else {
-        $ocrx_word.addClass('transparent');
-      }
-    });
 
-    $ocrx_word.on("click", function(e) {
-      var $this = $(this);
-      $this.toggleClass('transparent');
-    });
-  },2000);
+  var $ocrx_word = $('.ocrx_word');
+  
+  $('.hocr_toggle').on("click", function() {
+    if($ocrx_word.hasClass('transparent')) {
+      $ocrx_word.removeClass('transparent');
+    }
+    else {
+      $ocrx_word.addClass('transparent');
+    }
+  });
+
+  $ocrx_word.on("click", function(e) {
+    var $this = $(this);
+    $this.toggleClass('transparent');
+  });
+
 });
