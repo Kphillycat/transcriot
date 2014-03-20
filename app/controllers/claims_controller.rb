84be93b5589bc6a1a776db7193e1fbadd1fc1d04
Kate <katepdonahue@gmail.com>
2014-03-20 16:43:57 -0400

    Extra save

--- app/controllers/claims_controller.rb
+++ app/controllers/claims_controller.rb
@@ -28,7 +28,8 @@ class ClaimsController < ApplicationController
     claim.update_examiner(examiner_params)
     claim.update_damages(damages_params)
     claim.update_page_numbers(params["page"]["start_page_number"])
-
+    claim.save
+    puts "I just saved the claim"
     redirect_to :controller=>'claims', :action => 'edit', :id => claim.id
   end
 
